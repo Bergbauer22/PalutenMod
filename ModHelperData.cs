0c3f8d6c8a09f351122f0fa82df28af9395f7a06
Bergbauer22 <109031106+Bergbauer22@users.noreply.github.com>
2023-08-11 19:17:38 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace PalutenMod;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "36.1";
+    public const string WorksOnVersion = "38.1";
     public const string Version = "1.0.0";
     public const string Name = "PalutenMod";
 
